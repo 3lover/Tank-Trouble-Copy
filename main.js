5f961f70925460a9b03774531b73a72983176aca
Glitch (glitch-blank-website) <none>
2021-08-27 14:11:05 +0000

    😹🍦 Checkpoint ./index.html:18138957/92 ./main.js:18138957/628

--- main.js
+++ main.js
@@ -278,37 +278,37 @@ class Entity {
 				let obj = this;
 				let other = entities[j];
 				if (obj.id === other.id) continue;
-        let x1 = other.x - other.width/2,
+        /*let x1 = other.x - other.width/2,
             x2 = other.x + other.width/2,
             y1 = other.y - other.height/2,
             y2 = other.y + other.height/2;
         console.innerHTML = x1 + " : " + x2 + " : " + y1 + " : " + y2 + "<br>"
         console.innerHTML += obj.x + " : " + obj.y
         if (intersectRect(obj.x, obj.y, x1, x2, y1, y2) || //checks if circle is in rect
-            intersectCircle(obj.x, obj.y, obj.width ,x1, y1, x2, y2))//checks if left top corner in circle
-        return other.id;
-    /*var distX = Math.abs(obj.x - other.x - other.width / 2);
+            intersectRect(obj.x, obj.y, obj.width ,x1, y1, x2, y2))//checks if left top corner in circle
+        return other.id;*/
+    var distX = Math.abs(obj.x - other.x - other.width / 2);
     var distY = Math.abs(obj.y - other.y - other.height / 2);
-
-    if (distX > (other.width / 2 + obj.radius)) {
-        return false;
+console.innerHTML = distX + " : " + distY + " : " + (obj.width/2)**2
+    if (distX > (other.width / 2 + (obj.width/2))) {
+        return null;
     }
-    if (distY > (other.height / 2 + obj.radius)) {
-        return false;
+    if (distY > (other.height / 2 + (obj.width/2))) {
+        return null;
     }
 
     if (distX <= (other.width / 2)) {
-        return true;
+        return other.id;
     }
     if (distY <= (other.height / 2)) {
-        return true;
+        return other.id;
     }
 
     var dx = distX - other.width / 2;
     var dy = distY - other.height / 2;
-    if (dx * dx + dy * dy <= (obj.radius * obj.radius))
-      return other.id;*/
-				//if (Math.sqrt((other.x - obj.x) * (other.x - obj.x) + (other.y - obj.y) * (other.y - obj.y)) < obj.width) return other.id;
+    if (dx * dx + dy * dy <= ((obj.width/2) **2))
+      return other.id;
+		console.innerHTML = distX + " : " + distY + " : " + dx + " : " + dy + " : " + (obj.width/2)**2
 			}
 			return null
 		}
